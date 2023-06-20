0be4e3b8752e38c7e601826389607d3c51d0c461
Matheus Ferraz <matheus.ferraz2000@gmail.com>
2023-06-19 22:43:28 -0300

    Iniciando o Fiap donation system

---
+++ FiapDonationSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FiapDonationSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bem vindo ao FIAP donation system! Digite as informações que serão requisitadas abaixo: ");
+
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu telefone: ");
+            string telefone = Console.ReadLine();
+
+            Console.WriteLine("Digite seu e-mail: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Digite sua senha: ");
+            string senha = Console.ReadLine();
+
+            Console.WriteLine("Digite seu apelido: ");
+            string apelido = Console.ReadLine();
+
+        }
+    }
+}
