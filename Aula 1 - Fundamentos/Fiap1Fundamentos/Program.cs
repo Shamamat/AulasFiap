ba53a7ad7177b7720176dfbb662c2520bd6c37c9
Matheus Ferraz <45341918+Shamamat@users.noreply.github.com>
2023-06-19 23:17:35 -0300

    Aula 1

---
+++ Aula 1 - Fundamentos/Fiap1Fundamentos/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Fiap1Fundamentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor, dolar = 4.78;
+
+            Console.Write("Digite o valor de dólar que quer converter para real: ");
+            valor = Convert.ToDouble(Console.ReadLine());
+
+            double calcularValor = dolar * valor;
+
+            Console.WriteLine($"O valor digitado foi {valor} e convertido em real fica: ${calcularValor}");
+
+        }
+    }
+}
